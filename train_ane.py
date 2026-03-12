4c31da3b3d48547ef023bbb1d97dce52c616288e
Paul Thrasher <thrashr888@gmail.com>
2026-03-11 23:11:12 -0700

    Revert "Reduce ANE width to 40 aspect ratio"
    This reverts commit 47159abdd7fda4fe9fefc2d326e0aef74bcb0fa1.

--- train_ane.py
+++ train_ane.py
@@ -33,7 +33,7 @@ from prepare import CACHE_DIR, get_token_bytes
 TIME_BUDGET_SECONDS = 300
 
 # Model architecture
-ASPECT_RATIO = 40
+ASPECT_RATIO = 48
 HEAD_DIM = 64
 DEPTH = 8
 HIDDEN_DIM_MULTIPLE = 128
